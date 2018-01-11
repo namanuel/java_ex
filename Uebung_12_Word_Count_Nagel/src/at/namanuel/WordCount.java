7adbd687e4fa3de062934ed1299c93cd8a1c168c
namanuel <nagel.manuel@gmx.at>
2018-01-11 01:03:43 +0100

    Uebung 12 Fertig
    Funktioniert

--- Uebung_12_Word_Count_Nagel/src/at/namanuel/WordCount.java
+++ Uebung_12_Word_Count_Nagel/src/at/namanuel/WordCount.java
@@ -1,23 +1,22 @@
 package at.namanuel;
 
-import java.util.Map;
+
+import java.io.FileReader;
+import java.util.*;
 
 /**
  * Count occurrences of each word in a file.
- *
+ * <p>
  * <P>This program counts each occurrence of a word found
  * in a file and print the results of the search in tabular
  * form to the terminal.
- *
+ * <p>
  * <P>Usage:
+ * <p>
+ * <P><b>java</b> WordCount `file-name`
  *
- *   <P><b>java</b> WordCount `file-name`
- *
- * @param	args	Terminal Arguments.
- *
- * @author 	Manuel Nagel
+ * @author Manuel Nagel
  * @version 1.0
- *
  */
 public class WordCount {
 
@@ -28,50 +27,76 @@ public class WordCount {
         // to store all found words (Key) and the number of their
         // occurrences (Value).
         //
-        Map<String,Integer> words;
-        String input = "";
+        Map<String, Integer> words = new HashMap<>();
+        String line;
         try {
 
             // TODO (2)
             // Setup file input handling using the first
             // terminal argument.
             //
-            input = args[0];
+            FileReader fr = new FileReader(args[0]);
 
-            // TODO (3)
-            // Read all words from the file.
-            //
+            Scanner sr = new Scanner(fr);
 
+            while (sr.hasNext()) {
+                line = sr.nextLine();
 
-
-
-            // TODO (4)
-            // Drop all words that have less than two letters.
-            //
-
-            // TODO (5.1)
-            // Increment `Map` value, if the respective word (Key)
-            // is already present or ...
-            //
-
-
+                if (!line.trim().equals("")) {
+                    // TODO (3)
+                    // Read all words from the file.
+                    //
+                    String[] helper = line.split(" ");
+                    for (String word : helper) {
+                        if (word == null || word.trim().equals("") || word.length() == 1) {
+                            // TODO (4)
+                            // Drop all words that have less than two letters.
+                            //
+                            //word (.length)
+                            continue;
+                        }
+                        // TODO (5.2)
+                        // ... create a new `Map` entry with the
+                        // initial count.
+                        //
+                        String processed = word.toLowerCase();
+                        processed = processed.replaceAll("[\\W0-9]", "");
+                        if (words.containsKey(processed)) {
+                            words.put(processed, words.get(processed) + 1);
+                        } else {
+                            words.put(processed, 1);
+                        }
+                    }
+                }
+            }
+            //System.out.println("Unsorted: " + words);
             // TODO (5.2)
             // ... create a new `Map` entry with the
             // initial count.
             //
-
-
-            // TODO (6)
-            // Print the results in tabular form.
+            List<Map.Entry<String, Integer>> list = new LinkedList<Map.Entry<String, Integer>>(words.entrySet());
+            Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
+                @Override
+                public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+                    return o1.getValue().compareTo(o2.getValue());
+                }
+            });
 
             // TODO (6.1)
             // Print the table header.
             //
-
-
-            // TODO (6.2)
-            // Print every entry (key + value).
-            //
+            System.out.println("|----------------------------------|");
+            System.out.println("| Word                     |  Count|");
+            System.out.println("|----------------------------------|");
+            for(Map.Entry<String, Integer> item : list){
+                // TODO (6)
+                // Print the results in tabular form.
+                // TODO (6.2)
+                // Print every entry (key + value).
+                //
+                System.out.printf("| %-24s |     %02d|\n", item.getKey(), item.getValue());
+            }
+            System.out.println("|----------------------------------|");
 
             // TODO (7)
             // Clean up.
@@ -82,9 +107,11 @@ public class WordCount {
 
 
         } catch (Exception e) {
-
+            e.printStackTrace();
         }
-
     }
-
 }
+
+
+
+
